502f67965996fca8dbd38a02402edd886a905023
Van Nguyen <vnguyen94@gmail.com>
2016-03-10 00:44:35 -0800

    add default params, simplify/modularize features

--- KaggleRandomForestTreeRegressor.py
+++ KaggleRandomForestTreeRegressor.py
@@ -7,7 +7,7 @@ from sklearn import svm
 
 
 class KaggleRandomForestTreeRegressor:
-    def __init__(self, X=None, Y=None):
+    def __init__(self, X=None, Y=None, DEFAULT_PARAMS={}):
         if X is None:
             X = np.genfromtxt('data/kaggle.X1.train.fragment.txt', delimiter=',')
         if Y is None:
@@ -15,6 +15,7 @@ class KaggleRandomForestTreeRegressor:
 
         # split data set 75/25
         self.Xtr, self.Xte, self.Ytr, self.Yte = splitData(X, Y, 0.75)
+        self.DEFAULT_PARAMS = DEFAULT_PARAMS
 
     def _output_result_stats(self, feature_arr, error_arr):
         lowest_index, lowest_value = min(enumerate(error_arr), key=lambda p: p[1])
@@ -30,19 +31,20 @@ class KaggleRandomForestTreeRegressor:
         plt.ylabel('Error')
         plt.show()
 
-    def _create_random_forest(self, feature, feature_name):
-        params_svm = {feature_name.lower(): feature}
+    def _create_random_forest(self, feature_name, feature):
+        current_param = { feature_name: feature }
+        combined_param = dict(self.DEFAULT_PARAMS, **current_param)
         clf = RandomForestRegressor()
-        clf.set_params(**params_svm)
+        clf.set_params(**combined_param)
         clf = clf.fit(self.Xtr, self.Ytr)
 
         return clf
 
-    def test_feature(self, feature_arr, feature_name):
+    def test_feature(self, feature_name, feature_arr):
         error_arr = []
 
         for feature in feature_arr:
-            clf = self._create_random_forest(feature, feature_name)
+            clf = self._create_random_forest(feature_name, feature)
             score = clf.score(self.Xte, self.Yte)
             error_arr.append(score)
 
--- __init__.py
+++ __init__.py
@@ -10,13 +10,25 @@ np.random.seed(SEED_VALUE)
 X = np.genfromtxt('data/kaggle.X1.train.fragment.txt', delimiter=',')
 Y = np.genfromtxt('data/kaggle.Y.train.fragment.txt', delimiter=',')
 M, N = X.shape
-K = KaggleRandomForestTreeRegressor(X, Y)
+DEFAULT_PARAMS = {
+    'max_features': 40,
+    'max_depth': 15,
+    'min_samples_split': 2,
+    'min_samples_leaf': 11
+}
+
+K = KaggleRandomForestTreeRegressor(X, Y, DEFAULT_PARAMS)
 
 
-# EXECUTION
+# TESTING
 
-K.test_feature([i for i in range(1, 50, 5)], 'N_ESTIMATORS')
-K.test_feature([i for i in range(1, N, 5)], 'MAX_FEATURES')
-K.test_feature([i for i in range(1,20)], 'MAX_DEPTH')
-K.test_feature([i for i in range(1, 20)], 'MIN_SAMPLES_SPLIT')
-K.test_feature([i for i in range(1, 20)], 'MIN_SAMPLES_LEAF')
+features = [
+    ['n_estimators', [i for i in range(1, 50, 5)]],
+    # ['max_features', [i for i in range(1, N, 5)]],
+    # ['max_depth', [i for i in range(1, 20)]],
+    # ['min_samples_split', [i for i in range(1, 20)]],
+    # ['min_samples_leaf', [i for i in range(1, 20)]],
+]
+
+for feature in features:
+    K.test_feature(feature[0], feature[1])
