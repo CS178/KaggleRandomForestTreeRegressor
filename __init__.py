dbeb444ca880b47876a6fb2ed04416eb905d59a7
Van Nguyen <vnguyen94@gmail.com>
2016-03-10 10:21:35 -0800

    get predictions from test data

--- __init__.py
+++ __init__.py
@@ -16,20 +16,20 @@ K = KaggleRandomForestTreeRegressor(X, Y)
 # TESTING
 
 params = {
-    'max_features': 40,
-    'max_depth': 15,
-    'min_samples_split': 2,
-    'min_samples_leaf': 11
+    # 'max_features': 40,
+    # 'max_depth': 15,
+    # 'min_samples_split': 2,
+    # 'min_samples_leaf': 11
 }
 
 K.set_params(params)
 
 features_to_test = [
     ['n_estimators', [i for i in range(1, 50, 5)]],
-    # ['max_features', [i for i in range(1, N, 5)]],
-    # ['max_depth', [i for i in range(1, 20)]],
-    # ['min_samples_split', [i for i in range(1, 20)]],
-    # ['min_samples_leaf', [i for i in range(1, 20)]],
+    ['max_features', [i for i in range(1, N, 5)]],
+    ['max_depth', [i for i in range(1, 20)]],
+    ['min_samples_split', [i for i in range(1, 20)]],
+    ['min_samples_leaf', [i for i in range(1, 20)]],
 ]
 
 for feature in features_to_test:
@@ -38,6 +38,7 @@ for feature in features_to_test:
 
 # PREDICTIONS
 
+Xeval = np.genfromtxt('data/kaggle.X1.test.txt', delimiter=',')
 params = {
     'n_estimators': 100,
     'max_features': 65,
@@ -46,4 +47,4 @@ params = {
 }
 
 K.set_params(params)
-K.write_predictions(X)
+K.write_predictions(Xeval)
